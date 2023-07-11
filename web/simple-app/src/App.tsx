655240f97144527bcf2eebbd26ec1292b3875526
Josh Thomas <jthoms1@gmail.com>
2023-07-11 12:20:44 +0800

    Add simple web appp

---
+++ web/simple-app/src/App.tsx
@@ -0,0 +1,33 @@
+import { IonApp, setupIonicReact } from "@ionic/react";
+import SimpleComponent from "./components/SimpleComponent";
+
+/* Core CSS required for Ionic components to work properly */
+import "@ionic/react/css/core.css";
+
+/* Basic CSS for apps built with Ionic */
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
+
+/* Optional CSS utils that can be commented out */
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
+
+/* Theme variables */
+import "./theme/variables.css";
+
+setupIonicReact();
+
+const App: React.FC = () => {
+  return (
+    <IonApp>
+      <SimpleComponent />
+    </IonApp>
+  );
+};
+
+export default App;
---
+++ web/simple-app/src/components/SimpleComponent.tsx
@@ -0,0 +1,44 @@
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonIcon,
+  IonButtons,
+  IonButton,
+} from "@ionic/react";
+import { chevronBack } from "ionicons/icons";
+import { dismissPlugin } from "../super-app";
+
+const SimpleComponent: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons>
+            <IonButton
+              onClick={() => {
+                dismissPlugin.dismiss();
+              }}
+            >
+              <IonIcon icon={chevronBack} />
+              Dashboard
+            </IonButton>
+          </IonButtons>
+          <IonTitle>Time Tracking</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Time Tracking</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <p>This is just a simple app</p>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default SimpleComponent;
