5692f97cedb7d29da518ff82593436db49ed3775
Carl Poole <carl@ionic.io>
2023-07-11 01:42:36 -0500

    Update supabaseApi.ts back to localhost for demo

--- web/supabaseApi/supabaseApi.ts
+++ web/supabaseApi/supabaseApi.ts
@@ -3,7 +3,7 @@ import { PTOApproval, TimesheetApproval, TimesheetRequest } from "./types";
 
 export type { Session } from "@supabase/supabase-js";
 
-const supabaseUrl = "http://10.0.2.2:54323";
+const supabaseUrl = "http://localhost:54321";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0";
 const serviceKey =
